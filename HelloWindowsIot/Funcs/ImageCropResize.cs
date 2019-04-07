c44cb192cf3ecc29e5bf5c82d981db6e72fc341e
Peter Karsten <pkarsten@live.de>
2019-04-07 21:09:32 +0200

    Image Uniform To FIll

---
+++ HelloWindowsIot/Funcs/ImageCropResize.cs
@@ -0,0 +1,82 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using Windows.UI.Xaml.Media.Imaging;
+
+//namespace HelloWindowsIot
+//{
+//    public class ImageHelper
+//    {
+
+//        /// <summary>
+//        /// 
+//        /// </summary>
+//        /// <param name="myPic">Downloaded Pic from onedrive </param>
+//        /// <returns></returns>
+//        public static async Task<BitmapImage> ResizedImage(BitmapImage myPic)
+//        {
+//            BitmapImage bitmapimage = new BitmapImage();
+
+//            try
+//            {
+                
+//            }
+//            catch
+//            {
+
+//            }
+//            finally
+//            {
+                
+//            }
+//            return bitmapimage;
+
+//        }
+
+//        private bool Is16By9(int height, int width)
+//        {
+           
+//        }
+
+//        // https://stackoverflow.com/questions/43311873/how-do-i-cut-a-picture-in-an-aspect-ratio-while-retaining-a-rectangle-inside
+//        //public testc()
+//        //{
+//        //    if (breiteNeu > hoeheNeu)
+//        //    {
+//        //        if (breiteNeu / hoeheNeu > 1.777)
+//        //        {
+//        //            Console.WriteLine("1");
+//        //            float hj = 0.5625f * breiteNeu;
+//        //            yNeu -= (int)((hj - hoeheNeu) / 2);
+//        //            hoeheNeu = (int)hj;
+//        //        }
+//        //        else
+//        //        {
+//        //            Console.WriteLine("2");
+//        //            float bj = 1.777f * hoeheNeu;
+//        //            xNeu -= (int)((bj - breiteNeu) / 2);
+//        //            breiteNeu = (int)bj;
+//        //        }
+//        //    }
+//        //    else
+//        //    {
+//        //        if (breiteNeu / hoeheNeu > 0.5625)
+//        //        {
+//        //            Console.WriteLine("3");
+//        //            float hj = 1.777f * breiteNeu;
+//        //            yNeu -= (int)((hj - hoeheNeu) / 2);
+//        //            hoeheNeu = (int)hj;
+//        //        }
+//        //        else
+//        //        {
+//        //            Console.WriteLine("4");
+//        //            float bj = 0.5625f * hoeheNeu;
+//        //            xNeu -= (int)((bj - breiteNeu) / 2);
+//        //            breiteNeu = (int)bj;
+//        //        }
+//        //    }
+//        //}
+//    }
+//}
